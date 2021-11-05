386633f5602e8a4b2b51ee7904428c9c1816fc76
luolongfei <luolongf@gmail.com>
2021-11-05 16:09:45 +0800

    送信成功回执

--- app/Console/MigrateEnvFile.php
+++ app/Console/MigrateEnvFile.php
@@ -133,22 +133,6 @@ class MigrateEnvFile extends Base
     }
 
     /**
-     * 获取版本号数字部分
-     *
-     * @param $rawVer
-     *
-     * @return string|null
-     */
-    public function getVerNum($rawVer)
-    {
-        if (preg_match('/(?P<ver_num>\d+(?:\.\d+)*)/i', $rawVer, $m)) {
-            return $m['ver_num'];
-        }
-
-        return null;
-    }
-
-    /**
      * 备份旧文件
      *
      * 如果目标文件已存在，将会被覆盖
