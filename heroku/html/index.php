5d342b30b43723b61d718219a7a166f9505d521e
ff <105342163+llfapp@users.noreply.github.com>
2022-08-16 17:01:07 +0800

    test

--- heroku/html/index.php
+++ heroku/html/index.php
@@ -2,9 +2,14 @@
 
 set_time_limit(0);
 
+ob_end_clean();
+ob_implicit_flush();
+
+header('X-Accel-Buffering: no');
+
 echo '<pre>';
 
-echo "Freenom 自动续期\n";
+echo "Freenom 自动续期\n\n";
 
 echo "开始执行\n\n";
 
