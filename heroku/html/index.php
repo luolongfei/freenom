22b33cf07640eb40c80c7869f9226fa26cdbb332
ff <105342163+llfapp@users.noreply.github.com>
2022-08-16 18:30:17 +0800

    验证执行权限

--- heroku/html/index.php
+++ heroku/html/index.php
@@ -5,6 +5,12 @@ set_time_limit(0);
 header('X-Accel-Buffering: no');
 
 echo '<pre>';
+
+$FF_TOKEN = $_GET('ff-token');
+if ($FF_TOKEN !== getenv('FF_TOKEN')) {
+    die('你没有权限触发执行');
+}
+
 echo "Freenom 自动续期\n\n";
 echo "开始执行\n\n";
 
