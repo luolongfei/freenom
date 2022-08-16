a48cfd4c2b50c6a2f1ac97731f49c06a75898c3d
ff <105342163+llfapp@users.noreply.github.com>
2022-08-16 16:48:26 +0800

    更新提示语

--- heroku/startup.sh
+++ heroku/startup.sh
@@ -8,9 +8,8 @@
 set -e
 
 echo '[Info] 检测到你当前在 Heroku 环境运行容器，此环境依赖外部请求触发脚本运行，请自行前往 https://uptimerobot.com 配置心跳任务'
+echo '[Info] 由于 Heroku 对 cron 的支持不友好，故当前脚本只能通过请求 url 触发执行'
 echo '[Info] 更多部署手顺请参考文档：https://github.com/luolongfei/freenom'
-
-php run
 
 # 替换端口变量
 envsubst '\$PORT' < /app/nginx.template.conf > /app/nginx.conf
