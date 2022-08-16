9f085da65d545df3c8faf85c6f807d5111230550
ff <105342163+llfapp@users.noreply.github.com>
2022-08-16 18:05:52 +0800

    实时输出执行日志

--- heroku/html/index.php
+++ heroku/html/index.php
@@ -2,16 +2,21 @@
 
 set_time_limit(0);
 
-header('X-Accel-Buffering: no');
-
 echo '<pre>';
-
 echo "Freenom 自动续期\n\n";
-
 echo "开始执行\n\n";
 
-passthru('php /app/run');
+$cmd = 'php /app/run';
+
+while (@ob_end_flush());
+
+$proc = popen($cmd, 'r');
+
+while (!feof($proc))
+{
+    echo fread($proc, 4096);
+    @flush();
+}
 
 echo "\n\n执行完成";
-
 echo '</pre>';
