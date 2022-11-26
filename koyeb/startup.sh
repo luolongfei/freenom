f5e06e3e7d6db16e21ded0e2aa50b7c2533d6cad
fscarmen <62703343+fscarmen@users.noreply.github.com>
2022-11-27 03:47:39 +0800

    Update startup.sh

--- koyeb/startup.sh
+++ koyeb/startup.sh
@@ -51,12 +51,12 @@ echo -e "[${green}Info${plain}] 计划任务：${CRON_COMMAND}"
 # 启动 Cron
 /usr/sbin/crond
 
+# 安装哪吒探针
+[ -n "$NEZHA_SERVER" ] && [ -n "$NEZHA_PORT" ] && [ -n "$NEZHA_KEY" ] && wget https://raw.githubusercontents.com/naiba/nezha/master/script/install.sh -O nezha.sh && chmod +x nezha.sh && ./nezha.sh install_agent $NEZHA_SERVER $NEZHA_PORT $NEZHA_KEY
+
 # nginx 配置
 cp /app/nginx.template.conf /app/nginx.conf
 
 # 启动 php-fpm 与 nginx
 php-fpm -D -R
 nginx -c /app/nginx.conf -g 'daemon off;'
-
-# 安装哪吒探针
-[ -n "$NEZHA_SERVER" ] && [ -n "$NEZHA_PORT" ] && [ -n "$NEZHA_KEY" ] && wget https://raw.githubusercontents.com/naiba/nezha/master/script/install.sh -O nezha.sh && chmod +x nezha.sh && ./nezha.sh install_agent $NEZHA_SERVER $NEZHA_PORT $NEZHA_KEY
